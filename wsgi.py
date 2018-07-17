7245f19612bfc73c150525227b44326b6bd60d67
Erinn Looney-Triggs <erinn.looneytriggs@gmail.com>
2018-07-17 02:28:11 -0600

    More wsgi work.

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,6 @@ from app import app as application
 from app import db
 from app.db_models import Certificate, Principles
 
-@app.shell_context_processor
+@application.shell_context_processor
 def make_shell_context():
     return {'db': db, 'Certificate': Certificate, 'Principles': Principles}
